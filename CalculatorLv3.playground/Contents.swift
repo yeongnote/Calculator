a8115e6836aa4e98cabad52b0109926530191ebf
yeongnote <eongh0640@gmail.com>
2023-12-02 17:06:07 +0900

    Calculator클래스 수정

--- CalculatorLv3.playground/Contents.swift
+++ CalculatorLv3.playground/Contents.swift
@@ -2,7 +2,7 @@ import UIKit
 
 // Lv3 : AddOperation(더하기), SubstractOperation(빼기), MultiplyOperation(곱하기), DivideOperation(나누기) 연산 클래스를을 만든 후 클래스간의 관계를 고려하여 Calculator 클래스와 관계를 맺기
 // 추가사항: 클래스의 책임(단일책임원칙) 적용하기,
-
+/*
 //Calculator 클래스
 class Calculator {
     var firstNum: Double
@@ -12,45 +12,79 @@ class Calculator {
         self.firstNum = firstNum
         self.secondNum = secondNum
     }
+    
+    func calculate(operation: Operation) -> Double {
+        return operation.operate(firstNum: firstNum, secondNum: secondNum)
+    }
 }
-
+*/
+class Calculator {
+    private let addOperation: AddOperation
+    private let subtractOperation: SubstractOperation
+    private let multiplyOperation: MultiplyOperation
+    private let divideOperation: DivideOperation
+    
+    init(addOperation: AddOperation, subtractOperation: SubstractOperation, multiplyOperation: MultiplyOperation, divideOperation: DivideOperation){
+        self.addOperation = addOperation
+        self.subtractOperation = subtractOperation
+        self.multiplyOperation = multiplyOperation
+        self.divideOperation = divideOperation
+    }
+    
+    func calculate(operator: String, firstNum: Int, secondNum: Int) -> Double? {
+        switch `operator` {
+        case "+":
+            return addOperation.operate(firstNum: firstNum, secondNum: secondNum)
+        case "-":
+            return subtractOperation.operate(firstNum: firstNum, secondNum: secondNum)
+        case "*":
+            return multiplyOperation.operate(firstNum: firstNum, secondNum: secondNum)
+        case "/":
+            return divideOperation.operate(firstNum: firstNum, secondNum: secondNum)
+        default:
+            return nil
+        }
+    }
+}
 //덧셈 클래스
 class AddOperation {
-    func add(firstNum: Double, secondNum: Double) -> Double{
-        return firstNum + secondNum
+    func operate(firstNum: Int, secondNum: Int) -> Double{
+        return Double(firstNum + secondNum)
     }
 }
 
 //뺏셈 클래스
 class SubstractOperation {
-    func sub(firstNum: Double, secondNum: Double) -> Double{
-        return firstNum - secondNum
+    func operate(firstNum: Int, secondNum: Int) -> Double{
+        return Double(firstNum - secondNum)
     }
 }
 
 //곱셈 클래스
 class MultiplyOperation {
-    func multiply(firstNum: Double, secondNum: Double) -> Double{
-        return firstNum * secondNum
+    func operate(firstNum: Int, secondNum: Int) -> Double{
+        return Double(firstNum * secondNum)
     }
 }
 
 //나눗셈 클래스
 class DivideOperation {
-    func divide(firstNum: Double, secondNum: Double) -> Double{
-        return firstNum / secondNum
+    func operate(firstNum: Int, secondNum: Int) -> Double{
+        return Double(firstNum / secondNum)
     }
 }
 
+
+
 //프로퍼티 초기화
-let calculator = Calculator(firstNum: 0, secondNum: 0)
+let calculator = Calculator(addOperation: AddOperation(), subtractOperation: SubstractOperation(), multiplyOperation: MultiplyOperation(), divideOperation: DivideOperation())
 
-let addResult = AddOperation()
-let subResult = SubstractOperation()
-let multiplyResult = MultiplyOperation()
-let divideResult = DivideOperation()
+//옵셔널 기본값 설정
+let addResult = calculator.calculate(operator: "+", firstNum: 30, secondNum: 30) ?? 0
+let subResult = calculator.calculate(operator: "-", firstNum: 40, secondNum: 20) ?? 0
+let multiplyResult = calculator.calculate(operator: "*", firstNum: 2, secondNum: 9) ?? 0
+let divideResult = calculator.calculate(operator: "/", firstNum: 30, secondNum: 20) ?? 0
 
-print("덧셈 값: \(addResult.add(firstNum: 30, secondNum: 10))")
-print("빼셈 값: \(subResult.sub(firstNum: 10, secondNum: 5))")
-print("곱셉 값: \(multiplyResult.multiply(firstNum: 10, secondNum: 5))")
-print("나눗셈 값: \(divideResult.divide(firstNum: 10, secondNum: 5))")
+
+
+
